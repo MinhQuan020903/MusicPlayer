90327cc203c34c3f5a2fae48acbc0c37b199dc42
Your Name <doquan020903@gmail.com>
2022-10-25 10:26:24 +0700

    Setup Music Service, Firebase Music Source

--- app/src/main/java/com/example/musicplayer/di/ServiceModule.kt
+++ app/src/main/java/com/example/musicplayer/di/ServiceModule.kt
@@ -1,6 +1,7 @@
 package com.example.musicplayer.di
 
 import android.content.Context
+import com.example.musicplayer.data.remote.MusicDatabase
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.audio.AudioAttributes
@@ -15,6 +16,10 @@ import dagger.hilt.android.scopes.ServiceScoped
 @Module
 @InstallIn(ServiceComponent::class)
 object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideMusicDatabase() = MusicDatabase()
 
     @ServiceScoped
     @Provides
